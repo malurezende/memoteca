43bf678ec53c05d6c5bf2df12e1a3494068a0286
Malu Rezende <maluar@gmail.com>
2022-12-12 12:00:28 -0300

    feat: implemented backend json.service

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,22 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo info para componente filho',
-      autoria: 'componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-      autoria: '',
-      modelo: 'modelo1'
-  },
-  ]
+  listaPensamentos: Pensamento[]= [];
 
 }
